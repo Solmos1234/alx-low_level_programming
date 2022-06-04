9a98ec809e1433ed49aba4bffa29d903a91b9d08
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-04 06:29:50 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 int main(void)
-/** 
- *** main.Entry point
- ***	
- ***Return: Always 0 (success)
- * **/	
 { 
-puts("\"Programming is like building 0 multilingual puzzle");
-return(0);
+		puts("\"Programming is like building 0 multilingual puzzle");
+			return(0);
 }
