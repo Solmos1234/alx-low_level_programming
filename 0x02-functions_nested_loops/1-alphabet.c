7b86fb462c5ab4752c9083cd30c4900579dbcca3
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-08 01:00:53 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+* main - check the code
+*
+* Return: Always 0
+*/
+int main()
+{
+	char ch;
+	printf("Lowercase letters\n");
+	for (ch ='a'; ch<='z'; ch++)
+	{
+		printf("%c", ch);
+	}
+	return 0;
+}
