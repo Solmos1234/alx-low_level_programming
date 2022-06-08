917bf4834ff51a64d5d6fa86ea4f51ac436ca258
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-08 06:48:32 -0700

    putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
- /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0 (Success)
- */
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0 (Success)
+*/
 void print_alphabet(void)
 {
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar(i)
+		_putchar(i);
 	}
-	_putchar("\n")
+	_putchar("\n");
 }
