3e1f5143f85b7b6491626e0fbea556d47683f679
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-09 05:13:31 -0700

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 /**
-*main : this is where execution starts
-*Return : Always 0 (success)
+*This is code prints putchar 
+*main: this is where execution starts
+*Return: Always 0 (success)
 */
 int main(void)
 /**
-*main : execution starts
-*return : 0 (Success)
+*main: execution starts
+*return: 0 (Success)
 *description for return
 */
 {
+/**
+*Return: Always 0 Success
+*/
 	printf("_putchar\n");
 		return (0);
 }
