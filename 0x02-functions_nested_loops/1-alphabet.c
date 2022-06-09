7b9dad43b70d9ac5fd386d9b2c236c4ac6883575
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-09 05:27:17 -0700

    Task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
+#include"main.h"
 /**
-*main - check the code for Holberton School students.
+*print_alphabet - prints lowercase letters
 *
-* Return: Always 0 (Success)
+*main: check the code for Holberton School student
+*Return: Always 0 (Success)
 */
+
 void print_alphabet(void)
 {
 	int i;
-
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		putchar(i);
+		_putchar(i);
 	}
-	putchar("\n");
-	return (0);
+	_putchar("\n");
 }
