78c953682849def6e544187277ad0d52f90d5f66
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-14 16:10:49 -0700

    task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,9 +11,8 @@ int main()
 	int n;
 
 	n = 402;
-	_putchar("n = %d\n", n);
+	_putchar("n=%d\n", n);
 	reset_to_98(&n);
-	*n = 98;
-	_putchar("n = %d\n", n);
+	_putchar("n=%d\n", n);
 	return (0);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define _main_h_
 
 int _putchar(char n);
-void reset_to_98(int *n);
+void reset_to_98(int *);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
