da116a0a81ce5e00afe52b18808a276f1d4a141d
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-14 15:05:26 -0700

    Task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,9 +10,9 @@ int main(void)
 	int n;
 
 	n = 402;
-	printf("n = %d\n", n);
+	_putchar("n = %d\n", n);
 	reset_to_98(&n);
 	*n = 98;
-	printf("n = %d\n", n);
+	_putchar("n = %d\n", n);
 	return (0);
 }
