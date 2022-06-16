89db26e5e747108c36dec9063f2dabb94bee73c7
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 13:25:18 -0700

    Task7

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,4 +4,4 @@ int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *scr, int n);
 void reverse_array(int *a, int n);
 char *cap_string(char *);
-char *leet(char *s);
+char *leet(char *);
