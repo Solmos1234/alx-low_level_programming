763e7eca8ba0f40fe91b2894dc67ef751683a941
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 06:49:50 -0700

    Task4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *reverse_array - reverses the contents of integer arrays
+ *@a: Array of integers
+ *@n: Number of swap elements
+ *
+ *Return: empty
+ */
+void reverse_array(int *a, int n)
+{
+int *p, i, aux, k;
+p = a;
+for (i = 1; i <= n; i++)
+{
+p++
+}
+for (k = 0; k < i / 2; k++)
+{
+aux = a[k];
+a[k] = *p;
+*p = aux;
+p--;
+}
+}
