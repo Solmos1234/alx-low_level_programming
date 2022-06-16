aaa11cfdb8e32f79a4d7894f62abc7a82644cf83
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 05:28:44 -0700

    Task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,16 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int a = 0, b = 0;
-while (src[b])
+while	(src[b])
 {
 b++;
 }
-while (a < n && src[a])
+while	(a < n && src[a])
 {
 dest[a] = src[a];
 a++;
 }
-while (a < n)
+while	(a < n)
 {
 dest[a] = '\0';
 a++;
