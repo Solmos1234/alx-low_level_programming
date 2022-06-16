ea2e0788a52b38f1b797b8abd829a204ffcad2a2
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 14:22:00 -0700

    Task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * string_toupper - changes lowercase to uppercase
- * @p: the string shall be modified
+ *string_toupper - changes lowercase to uppercase
+ *@p: the string shall be modified
  *
- * Return: char var
+ *Return: char var
  */
-
 char *string_toupper(char *p)
 {
 int a = 0;
@@ -14,7 +13,7 @@ while (p[a])
 {
 if (p[a] >= 97 &&p[a] <= 122)
 {
-p[a] -= 32;
+p[a] - 32;
 }
 a++;
 }
