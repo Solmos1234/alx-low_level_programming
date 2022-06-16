6968ef42659d178a3e0c34813278d19b16cc563c
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 04:39:33 -0700

    Task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * strncpy - copying string
  * @dest: Destination
@@ -9,7 +11,7 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int a =0, b =0;
+int a = 0, b = 0;
 while (src[b])
 {
 b++;
