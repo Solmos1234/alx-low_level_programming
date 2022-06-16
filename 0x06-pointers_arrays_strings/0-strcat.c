d039dff1e882ef5fc8613f846ee569be14ecbb76
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 02:31:21 -0700

    Task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_strcat - combines two strings
+*@dest: string destination
+*@src: Is the source string
+*
+*Reeturn: A pointer to dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+	
+	for (i = 0; src[i] !=0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
+}
