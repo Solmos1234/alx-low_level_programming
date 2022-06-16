9d2655c71ab47b0531f713bc7e2fef76cf0daccf
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 16:40:29 -0700

    Task6

---
+++ 0x02-functions_nested_loops/abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_abs - A function that compute actual value of integer
+ *@ab: Integer input
+ *Return : absolute value of ab
+ */
+int _abs(int ab)
+{
+return (ab * ((ab > 0) - (ab < 0)));
+}
