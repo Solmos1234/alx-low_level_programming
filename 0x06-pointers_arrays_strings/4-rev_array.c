3bbb90eb6cdc17834ff73b2c2a2239ae42be5f6b
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 08:25:45 -0700

    Task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,25 +1,22 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  *reverse_array - reverses the contents of integer arrays
  *@a: Array of integers
  *@n: Number of swap elements
- *
  *Return: empty
  */
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
-	p = a;
-	for (i = 1; i <= n; i++)
+	int *p, i, temp;
+
+	for (i = 0; i < n -1; i++)
 	{
-	p++
-	}
-	for (k = 0; k < i / 2; k++)
-	{
-	aux = a[k];
-	a[k] = *p;
-	*p = aux;
-	p--;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
 	}
 }
