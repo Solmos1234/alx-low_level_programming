aa105732c948dcefd02cee6f6f86fb6b7f3ee254
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 19:55:28 -0700

    Task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,14 +7,14 @@
 */
 char *string_toupper(char *p)
 {
-int a = 0;
+int a =0;
 while (p[a])
 {
-if (p[a] >= 97 &&p[a] <= 122)
+if (p[a] >= 97 && p[a] < 122)
 {
 p[a] -= 32;
 }
-a++;
+a++
 }
 return (p);
 }
