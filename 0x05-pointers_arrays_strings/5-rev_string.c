cc5e8a8aee68c5658ef42aded5847b2d2d449b4c
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 19:01:17 -0700

    Task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *rev_string - function with single argmt
+ *@s: char tyep argmt
+ *Description: reversing string
+ *Return: None
+ */
+void rev_string(char *s)
+{
+int i, k, tmp, len;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+k = 0;
+len = i;
+i--;
+while (k < len / 2)
+{
+tmp = s[k];
+s[k] = s[i];
+s[i] = tmp;
+i--;
+k++;
+}
+}
