ecd4548c191e3db11b2d59ef92f563d3f7753954
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 20:43:41 -0700

    Task8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_square - print a square
+ *@size: size of the square
+ *description: _putchar can only be used
+ */
+void print_square(int size)
+{
+int x, y;
+y = 0;
+if (size < 1)
+_putchar('\n');
+while (y < size)
+{
+x = 0;
+while (x < size)
+{
+_putchar('#');
+x++;
+}
+_putchar('\n');
+y++;
+}
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,4 +4,4 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void print_triangle(int size);
-
+void print_square(int size);
