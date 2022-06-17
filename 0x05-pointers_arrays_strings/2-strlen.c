c4e7d4a8ceb39ed7b1fe35679291148478109100
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 18:33:38 -0700

    Task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strlen - function with 1 argument
+ *@s: char type pointer
+ *
+ *Description: strlen similar func
+ *Return: string
+ */
+int _strlen(char *s)
+{
+int i;
+i = 0;
+while (*(s + i ) != '\0')
+{
+i++;
+}
+return (i);
+}
