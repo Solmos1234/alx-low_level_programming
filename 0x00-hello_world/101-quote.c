a8b9de671deba1546752a6f44b7d998556283ba8
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-17 03:32:28 -0700

    Task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - prints ou the last part of the quote in starndard error
+ *Return: 1 if success
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
