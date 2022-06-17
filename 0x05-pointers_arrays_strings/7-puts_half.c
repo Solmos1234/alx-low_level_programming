feaaf332771626befaa624e563bf28b82bdde6c4
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-17 15:31:32 -0700

    Task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *puts_half - prints second half string
+ *@str: string to use
+ */
+void puts_half(char *str)
+{
+int length, i, n;
+for (length = 0; str[length] != '\0'; length++)
+{
+}
+n = (length - 1) / 2;
+for (i = n + 1; str[i]; != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
