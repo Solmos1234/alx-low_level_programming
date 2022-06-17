372debb42ee2bc991ba935a6685e1783692bed5d
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-17 03:54:34 -0700

    Task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
  *main - prints ou the last part of the quote in starndard error
- *Return: 1 if success
+ *Return: Always 0
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
