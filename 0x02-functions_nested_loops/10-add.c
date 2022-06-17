a82b43fc7371f6d2debc2bd091d36ec1d818ba6c
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-17 06:40:01 -0700

    Task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *add - adds two integers
+ *@a: value to add to b
+ *@b: value to be added to a
+ *Return: sum of two integers
+ */
+int ad(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
