a1637c9feacf5115fb5ee3d7ae6d4e6ae3c28d8e
Solmos1234 <kipkirui.solomon44@gmail.com>
2022-06-16 18:26:15 -0700

    Task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *swap_int - function with two type pointer argmt
+ *@a: 1st argmt int type
+ *@b: 2nd argmt int type
+ *
+ *Description: using ponter to change values
+ *Return: void
+ */
+void swap_int(int *a, int*b)
+{
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
+}
